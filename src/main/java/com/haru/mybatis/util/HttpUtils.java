7a6d8c6e42855c94e017dc1aa14d76f657c69218
HAHARURU <18055394367@163.com>
2018-07-05 15:14:39 +0800

    换行测试

--- src/main/java/com/haru/mybatis/util/HttpUtils.java
+++ src/main/java/com/haru/mybatis/util/HttpUtils.java
@@ -63,6 +63,7 @@ public class HttpUtils {
             int code = client.executeMethod(method);
             resStream = method.getResponseBodyAsStream();
             br = new BufferedReader(new InputStreamReader(resStream, respCharset));
+
             StringBuffer resBuffer = new StringBuffer();
             String resTemp = "";
             while ((resTemp = br.readLine()) != null) {
